315579640bf12d5af0524f089791f0e3b74423b3
LAPTOP-HE20GUS5\carlo <curlirojo@gmail.com>
2020-04-22 03:38:12 +0200

    Add comment

--- src/LoadFiles/LoadFile.ts
+++ src/LoadFiles/LoadFile.ts
@@ -5,6 +5,7 @@ export class LoaderFile {
     public constructor (protected path: string) {}
 
     public loadFile (): object {
+        //TODO: implement async multi-load
         let fileContent = {};
         if (this.existPath()) {
             const rawFileContent = readFileSync(this.path, 'utf8');
